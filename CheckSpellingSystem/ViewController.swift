1b45d32c620d00c74194f606ac2c2cd7e7432a95
jinunzzan <lovebox6607@gmail.com>
2023-07-18 10:49:05 +0900

    Initial Commit

---
+++ CheckSpellingSystem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckSpellingSystem
+//
+//  Created by Eunchan Kim on 2023/07/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
